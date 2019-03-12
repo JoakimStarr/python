a0f26255fefda0cd6d61735c3d14af3748a466ad
miskies <18385086034@163.com>
2019-03-12 21:48:32 +0800

    一些简单的算法

---
+++ algorithm/soundex.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Miskies
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: Miskies123@gmail.com
+@software: pycharm
+@file: soundex.py
+@time: 19-3-12 下午9:47
+@desc:
+'''
