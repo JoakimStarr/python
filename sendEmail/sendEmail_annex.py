dd88a1ad93e673370ee4e0d305d24ac2c2849410
miskies <18385086034@163.com>
2019-03-12 21:50:16 +0800

    邮件相关

--- sendEmail/sendEmail_annex.py
+++ sendEmail/sendEmail_annex.py
@@ -9,3 +9,31 @@
 @time: 19-3-12 下午9:15
 @desc:
 '''
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender = "starcloudscience@163.com"
+receiver = "18385086034@163.com"
+smtpserver = 'smtp.163.com'
+password = 'Miskies123'
+subject = 'Python send annex Email test'
+
+msgRoot = MIMEMultipart("mixed")
+msgRoot["Subject"] = "test email"
+
+#构造附件
+attr = MIMEText(open('./Sierra.jpg','rb').read(),'base64','utf-8')
+attr["Content-Type"] = 'application/octet-stream'
+attr["Content-Disposition"] = 'attachment;filename="Subject.jpg"'
+msgRoot.attach(attr)
+
+try:
+    smtp = smtplib.SMTP()
+    smtp.connect(smtpserver)
+    smtp.login(sender,password)
+    smtp.sendmail(sender,receiver,msgRoot.as_string())
+    print("发送成功")
+    smtp.quit()
+except smtplib.SMTPException:
+    print("ERROR：发送失败")
--- sendEmail/sendEmail_img.py
+++ sendEmail/sendEmail_img.py
@@ -9,3 +9,38 @@
 @time: 19-3-12 上午11:51
 @desc:
 '''
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+sender = 'starcloudscience@163.com'
+recerver = '18385086034@163.com'
+subject = 'python send image email test'
+smptserver = 'smtp.163.com'
+password = 'Miskies123'
+
+msgRoot = MIMEMultipart('related')
+msgRoot["Subject"] = 'test message'
+
+msgText = MIMEText(u'''
+    <h2>send html text and images<h2>
+    <img src="./Sierra.jpg"> 
+    ''','html','utf-8')
+msgRoot.attach(msgText)
+
+fp = open("./Sierra.jpg",'rb')
+msgImg = MIMEImage(fp.read())
+fp.close()
+
+msgImg.add_header("Content-ID",'')
+msgRoot.attach(msgImg)
+
+try:
+    smtp = smtplib.SMTP()
+    smtp.connect(smptserver)
+    smtp.login(sender,password)
+    smtp.sendmail(sender,recerver,msgRoot.as_string())
+    print("发送成功")
+    smtp.quit()
+except smtplib.SMTPException:
+    print("发送失败")
