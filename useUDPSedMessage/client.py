cbca062af63bc2f00f8e0845e9b19b7d9843341e
miskies <18385086034@163.com>
2019-03-15 16:05:53 +0800

    add file

---
+++ useUDPSedMessage/client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: miskies
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: miskies123@gmail.com
+@software: pycharm
+@file: client.py
+@time: 19-3-15 下午2:28
+@desc:
+'''
