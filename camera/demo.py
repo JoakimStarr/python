191264b5225eb6f98f517db2612559211b3e9917
miskies <18385086034@163.com>
2019-03-27 13:23:50 +0800

    相机

--- camera/demo.py
+++ camera/demo.py
@@ -9,3 +9,15 @@
 @time: 19-3-18 下午3:20
 @desc:
 ''' 
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+while(1):
+    ret, frame = cap.read()
+    cv2.imshow("captrue",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
